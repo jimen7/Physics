fc1bc611b2ff34c6ee3784463fe0078e15cce0d8
NAPIER-MAIL\40204497 <40204497@live.napier.ac.uk>
2018-10-19 12:40:41 +0100

    Did the drawing method for Cloth task, now on with the forces

--- project5/main.cpp
+++ project5/main.cpp
@@ -38,7 +38,8 @@ float rest = 0.5f;//spring rest length
 float kd = 5.0f;//damping coefficient
 
 
-const int particlenum = 100;//particle num per vertex
+const int particlenum = 10;//particle num per vertex
+const int vertnum = 10;
 //Drag d = Drag();
 
 
@@ -51,17 +52,15 @@ float RandomFloat(float a, float b) {
 }
 
 
-void seflagPos(std::vector<Particle> *pvert) {
-	vec3 inpos = vec3(-10.0f, -10.0f, 0.0f);
-	float gap = 0.5f;
-
-	for (unsigned int i = 0; i < particlenum; ++i) {
-		for (unsigned int x = 0; x%10==0 ; ++x) {
-			for (unsigned int y = 0; y % 10 == 0; ++y) {
-				(*pvert)[x].setPos(inpos + vec3(x*gap,0.0f,y*gap));
-			}		
-		}
+void seflagPos(std::vector<std::vector<Particle>> &pvert) {
+	vec3 inpos = vec3(-5.0f, 10.0f, 0.0f);
+	float gap = 1.0f;
+	for (unsigned int x = 0; x <10 ; ++x) {
+		for (unsigned int y = 0; y < 10; ++y) {
+			pvert[x][y].setPos(inpos + vec3(x*gap,0.0f,y*gap));
+		}		
 	}
+
 
 
 }
@@ -80,22 +79,33 @@ int main()
 	Shader lambert = Shader("resources/shaders/physics.vert", "resources/shaders/physics.frag");
 	plane.setShader(lambert);
 
-	//Create particles via class:
-	std::vector<Particle> allPart;
 
-	std::vector<std::vector<Particle>> vecvec;
 	Shader blue = Shader("resources/shaders/solid.vert", "resources/shaders/solid_blue.frag");
+	//Create particles via class:
+	//std::vector<Particle> allPart;
+	//for (unsigned int i = 0; i < particlenum; ++i) {
+	//	allPart.push_back(Particle::Particle());
+	//	allPart[i].getMesh().setShader(blue);
+	//}
+
+	//Create vertice of vertices
+	std::vector<std::vector<Particle>> vecvec(vertnum);
 	for (unsigned int i = 0; i < particlenum; ++i) {
-		allPart.push_back(Particle::Particle());
-		allPart[i].getMesh().setShader(blue);
+		for (unsigned int j = 0; j < vertnum; ++j) {
+			vecvec[j].push_back(Particle::Particle());
+			vecvec[j][i].getMesh().setShader(blue);
+		}
 	}
+
 
 
 	//Set particles initial velocity
 	for (unsigned int i = 0; i < particlenum; ++i) {
-		//allPart[i].setVel(vec3(RandomFloat(-10.0f, 10.0f), RandomFloat(-10.0f, 10.0f), RandomFloat(-10.0f, 8.0f)));
-		allPart[i].setVel(vec3(0.0f));
-		//allPart[i].setPos(vec3(RandomFloat(0.0f, 5.0f), RandomFloat(5.0f, 9.0f), RandomFloat(0.0f, 8.0f)));
+		for (unsigned int j = 0; j < vertnum; ++j) {
+			//vecvec[j][i].setVel(vec3(RandomFloat(-10.0f, 10.0f), RandomFloat(-10.0f, 10.0f), RandomFloat(-10.0f, 8.0f)));
+			vecvec[j][i].setVel(vec3(0.0f));
+			//vecvec[j][i].setPos(vec3(RandomFloat(0.0f, 5.0f), RandomFloat(5.0f, 9.0f), RandomFloat(0.0f, 8.0f)));
+		}
 	}
 
 	//Set particles' initial position
@@ -104,7 +114,7 @@ int main()
 	//	allPart[i].setPos(vec3(allPart[i-1].getPos())+vec3(2.0f,0.0f,0.0f));
 	//}
 
-	seflagPos(&allPart);
+	seflagPos(vecvec);             //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////FUNCTION HERE
 
 
 	// create demo objects (a cube and a sphere)
@@ -136,21 +146,23 @@ int main()
 	std::vector<Hooke*> Hookes;
 
 	//Adding forces to the Hook vector
-	for (int i = 0; i < HookeForces; i++)
-	{
-		Hookes.push_back(new Hooke(&(allPart[i]), &(allPart[i+1]), ks, kd, rest));
+	for (unsigned int j = 0; j < vertnum; j++) {
+		for (int i = 0; i < HookeForces; i++)
+		{
+			Hookes.push_back(new Hooke(&(vecvec[j][i]), &(vecvec[j][i]), ks, kd, rest));
+		}
 	}
 
-
 	//Adding the forces applied to the particle
-	//allPart[1].addForce(Hookes[0]);
-	//for (int i = 1; i < particlenum-2; i++) {
-	//	allPart[i].addForce(&g);
-	//	allPart[i].addForce(Hookes[i]);
-	//	allPart[i+1].addForce(Hookes[i]);
+	//for (unsigned int j = 0; j < vertnum; j++) {
+	//	vecvec[j][1].addForce(Hookes[0]);
+	//	for (int i = 1; i < particlenum-2; i++) {
+	//		vecvec[j][i].addForce(&g);
+	//		vecvec[j][i].addForce(Hookes[i]);
+	//		vecvec[j][i+1].addForce(Hookes[i]);
+	//	}
+	//	vecvec[j][8].addForce(Hookes[8]);
 	//}
-	//allPart[8].addForce(Hookes[8]);
-
 	
 
 
@@ -207,30 +219,32 @@ int main()
 			/*
 			**	SIMULATION
 			*/
-			for (unsigned int i = 1; i < particlenum; i++) {
+			for (unsigned int j = 0; j < vertnum; j++) {
+				for (unsigned int i = 1; i < particlenum; i++) {
 
-				allPart[i].setAcc(allPart[i].applyForces(allPart[i].getPos(), allPart[i].getVel(), t, dt));
+					vecvec[j][i].setAcc(vecvec[j][i].applyForces(vecvec[j][i].getPos(), vecvec[j][i].getVel(), t, dt));
 
-					/*for (unsigned int j = 0; j < 3; j++) {
-						if (allPart[i].getPos()[j] < cubecorner[j]) {
-							allPart[i].setVel(j, allPart[i].getVel()[j] * -1.0f);
-							allPart[i].setPos(j, cubecorner[j]);
+					/*for (unsigned int k = 0; k < 3; k++) {
+						if (vecvec[j][i].getPos()[k] < cubecorner[k]) {
+							vecvec[j][i].setVel(k, vecvec[j][i].getVel()[k] * -1.0f);
+							vecvec[j][i].setPos(k, cubecorner[k]);
 						}
-						else if (allPart[i].getPos()[j] > cubecorner[j] + d[j]) {
-							allPart[i].setVel(j, allPart[i].getVel()[j] * -1.0f);
-							allPart[i].setPos(j, cubecorner[j] + d[j]);
+						else if (vecvec[j][i].getPos()[k] > cubecorner[k] + d[k]) {
+							vecvec[j][i].setVel(k, vecvec[j][i].getVel()[k] * -1.0f);
+							vecvec[j][i].setPos(k, cubecorner[k] + d[k]);
 						}
 
 					}*/
+				}
 			}
 
+			for (unsigned int j = 0; j < vertnum; j++) {
+				for (unsigned int i = 1; i < particlenum; i++) {
 
-			for (unsigned int i = 1; i < particlenum; i++) {
-
-				allPart[i].setVel(allPart[i].getVel() + dt * allPart[i].getAcc());
-				allPart[i].translate(dt*allPart[i].getVel());
+					vecvec[j][i].setVel(vecvec[j][i].getVel() + dt * vecvec[j][i].getAcc());
+					vecvec[j][i].translate(dt*vecvec[j][i].getVel());
+				}
 			}
-
 
 
 			accumulator -= dt;
@@ -246,10 +260,11 @@ int main()
 		app.draw(plane);
 		// draw particles
 		//app.draw(particle1.getMesh());
-		for (unsigned int i = 0; i < particlenum; i++) {
-			app.draw(allPart[i].getMesh());
+		for (unsigned int j = 0; j < vertnum; j++) {
+			for (unsigned int i = 0; i < particlenum; i++) {
+				app.draw(vecvec[j][i].getMesh());
+			}
 		}
-
 		// draw demo objects
 		//app.draw(cube);
 		//app.draw(sphere);
