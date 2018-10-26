c52aeb4609d244f3402ac391de0599f00b0c64c7
NAPIER-MAIL\40204497 <40204497@live.napier.ac.uk>
2018-10-26 12:40:46 +0100

    Tried practical 6, went to trask 2, realized task 1 wasn't working properly, so now back to task 1

---
+++ project6/Force.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cmath>
+#include "Force.h"
+#include "Body.h"
+#include "glm/ext.hpp"
+
+float density = 1.225f;
+float aircoefficient = 1.05f;
+glm::vec3 area = glm::vec3(0.1f, 0.1f, 0.0f);
+//float ks = 0.25f; //Spring constant, how loose the sppring is
+//float kd = 1.0f; //Damping Factor
+//float springrest = 1.5f;
+
+
+Force::Force()
+{
+}
+
+
+Force::~Force()
+{
+}
+
+glm::vec3 Force::apply(float mass, const glm::vec3 &pos, const glm::vec3 &vel) {
+	return glm::vec3(0.0f);
+}
+
+/*
+** Gravity
+*/
+glm::vec3 Gravity::apply(float mass, const glm::vec3 &pos, const glm::vec3 &vel) {
+	//complete. should return the force from gravity		
+	glm::vec3 ForceG = mass * (glm::vec3(0.0f, -9.8f, 0.0f));
+	//glm::vec3 acceleration = ForceG / mass; //
+	return ForceG;
+}
+
+/*
+** Drag
+*/
+glm::vec3 Drag::apply(float mass, const glm::vec3 &pos, const glm::vec3 &vel) {
+	//complete. should return the force from aerodynamic Drag
+	float absoluteu = glm::length(vel);
+	glm::vec3 e = -vel / absoluteu;
+	glm::vec3 Fa = 0.5*density*absoluteu*absoluteu*aircoefficient*area*e;
+	//glm::vec3 acceleration = Fa / mass; // 
+	return Fa;
+}
+
+/*
+** Hooke's Law
+*/
+glm::vec3 Hooke::apply(float mass, const glm::vec3 &pos, const glm::vec3 &vel) {
+	//complete. should return the force from the Spring force
+
+	float length = glm::length(m_b2->getPos() - m_b1->getPos());
+	glm::vec3 e;
+	if (length == 0) {
+		e = glm::vec3(0.0f);
+	}
+	else {
+		e = (m_b2->getPos() - m_b1->getPos()) / length;
+	}
+	
+
+
+	float Fh = (-1.0f) * getks() * (getrest() - length); //x in this case is rest lenth -current length
+
+
+	float Fd = (-1.0f)*getkd() * (dot(m_b1->getVel(), e) - dot(m_b2->getVel(), e));
+	//glm::vec3 acceleration = (glm::vec3(0.0f, (Fh+Fd), 0.0f))/mass;
+
+	if (m_b2->getPos() == pos) {
+		return -(Fh+Fd)*e;
+	}
+	else {
+		return (Fh+Fd)*e;
+	}
+	
+	//return glm::vec3(0.0f, (Fh + Fd), 0.0f);
+}
+
+
+glm::vec3 Wind::apply(float mass, const glm::vec3 &pos, const glm::vec3 &vel) {
+	/*if (glm::length(m_b1->getVel()) > 3.0f) {
+		m_b1->setVel(m_b1->getVel() * 0.8f);
+	}*/
+	//glm::vec3 air = glm::vec3(1.2f,0.9f,0.0f);
+	glm::vec3 air = glm::vec3(10.2f, 0.0f, 0.0f);
+	glm::vec3 vsurface = (m_b1->getVel() + m_b2->getVel() + m_b3->getVel()) / 3.0f; //(v1+v2+v3)/3
+	float vlength = glm::length(vsurface); //	|vsurface|
+	if (vlength == 0) {
+		return glm::vec3(0.0f);
+	}
+	vsurface = vsurface - air;
+	glm::vec3 rightvector = cross(m_b2->getPos() - m_b1->getPos(), m_b3->getPos() - m_b1->getPos()); //(r3-r1)x(r2-r1)
+	glm::vec3 normal = rightvector /glm::length(rightvector); //rightvector normalized
+	float trianglearea = 0.5 * glm::length(rightvector); 
+	glm::vec3 crossarea = trianglearea * (((vsurface)*normal)/ vlength);
+
+	glm::vec3 Fw = -0.5f*density*vlength*vlength*aircoefficient*crossarea*normal;
+
+	return Fw;
+	
+}
---
+++ project6/main.cpp
@@ -0,0 +1,411 @@
+#pragma once
+// Math constants
+#define _USE_MATH_DEFINES
+#include <cmath>  
+#include <random>
+
+// Std. Includes
+#include <string>
+#include <time.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/matrix_operation.hpp>
+#include "glm/ext.hpp"
+
+// Other Libs
+#include "SOIL2/SOIL2.h"
+
+// project includes
+#include "Application.h"
+#include "Shader.h"
+#include "Mesh.h"
+#include "Body.h"
+#include "Particle.h"
+#include "Force.h"
+#include "RigidBody.h"
+using namespace glm;
+
+// time
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
+
+Gravity g = Gravity(glm::vec3(0.0f, -9.8f, 0.0f));
+
+float ks = 50.0f; //spring stiffness 
+float rest = 0.1f;//spring rest length
+float kd = 30.0f;//damping coefficient
+
+
+const int particlenum = 10;//particle num per vertex
+const int vertnum = 10;
+//Drag d = Drag();
+
+
+//Method tyhat creates random variables between 2 values
+float RandomFloat(float a, float b) {
+	float random = ((float)rand()) / (float)RAND_MAX;
+	float diff = b - a;
+	float r = random * diff;
+	return a + r;
+}
+
+
+void seflagPos(std::vector<std::vector<Particle>> &pvert) {
+	vec3 inpos = vec3(-5.0f, 10.0f, 0.0f);
+	float gap = 1.0f;
+	for (unsigned int x = 0; x <10 ; ++x) {
+		for (unsigned int y = 0; y < 10; ++y) {
+			pvert[x][y].setPos(inpos + vec3(x*gap, 0.0f ,-1.0f*y*gap));
+		}		
+	}
+}
+
+void seflagPoscloth(std::vector<std::vector<Particle>> &pvert) {
+	vec3 inpos = vec3(-5.0f, 10.0f, 0.0f);
+	float gap = 1.0f;
+	for (unsigned int x = 0; x < 10; ++x) {
+		for (unsigned int y = 0; y < 10; ++y) {
+			pvert[x][y].setPos(inpos + vec3(x*gap, -1.0f*y*gap, 0.0f ));
+			//std::cout << pvert[x][y].getPos()[0] <<";"<< pvert[x][y].getPos()[1] << ";" << pvert[x][y].getPos()[2] << std::endl;
+		}
+	}
+}
+
+
+void addHookeForces(std::vector<std::vector<Particle>> &pvert) {
+	for (unsigned int j = 0; j < vertnum; j++) {
+		for (int i = 0; i < particlenum-1; i++)
+		{
+			if (i != 0 && j != 0 && i != 9 && j != 9) {
+
+				Hooke* top = new Hooke(&(pvert[j][i]), &(pvert[j - 1][i]), ks, kd, rest);
+				Hooke* left = new Hooke(&(pvert[j][i]), &(pvert[j][i - 1]), ks, kd, rest);
+				Hooke* bottom = new Hooke(&(pvert[j][i]), &(pvert[j + 1][i]), ks, kd, rest);
+				Hooke* right = new Hooke(&(pvert[j][i]), &(pvert[j][i + 1]), ks, kd, rest);
+
+				pvert[j][i].addForce(&g);
+
+				pvert[j][i].addForce(top);
+				pvert[j][i].addForce(left);
+				pvert[j][i].addForce(bottom);
+				pvert[j][i].addForce(right);
+
+				vec3 topnormal = (cross((pvert[i-1][j-1].getPos()- pvert[i][j].getPos()),(pvert[i-1][j].getPos()- pvert[i][j].getPos())))/glm::length(cross((pvert[i - 1][j - 1].getPos() - pvert[i][j].getPos()), (pvert[i - 1][j].getPos() - pvert[i][j].getPos())));
+				float toparea = 0.5f * glm::length(cross((pvert[i - 1][j - 1].getPos() - pvert[i][j].getPos()), (pvert[i - 1][j].getPos() - pvert[i][j].getPos())));
+			}
+			
+
+
+		}
+	}
+}
+
+void addClothForces(std::vector<std::vector<Particle>> &pvert) {
+
+	for (unsigned int j = 0; j < vertnum; j++) {
+
+		for (int i = 0; i < particlenum; i++)
+		{
+	
+			if (j != 0 ) {
+
+				if (i == 0) {   ////LEFT SIDE
+					if (j == 9) { //Bottom left particle
+						Hooke* top = new Hooke(&(pvert[j][i]), &(pvert[j - 1][i]), ks, kd, rest);
+						Hooke* right = new Hooke(&(pvert[j][i]), &(pvert[j][i + 1]), ks, kd, rest);
+
+						pvert[j][i].addForce(&g);
+
+						pvert[j][i].addForce(top);
+						pvert[j][i].addForce(right);
+					}
+					else {
+						Hooke* top = new Hooke(&(pvert[j][i]), &(pvert[j - 1][i]), ks, kd, rest);
+						Hooke* right = new Hooke(&(pvert[j][i]), &(pvert[j][i + 1]), ks, kd, rest);
+						Hooke* bottom = new Hooke(&(pvert[j][i]), &(pvert[j + 1][i]), ks, kd, rest);
+
+						Wind * wind1 = new Wind(&(pvert[j][i]), &(pvert[j][i + 1]), &(pvert[j + 1][i])); //particle, particle right, particle down
+
+						pvert[j][i].addForce(&g);
+
+						pvert[j][i].addForce(top);
+						pvert[j][i].addForce(bottom);
+						pvert[j][i].addForce(right);
+
+
+						pvert[j][i].addForce(wind1);
+						pvert[j][i + 1].addForce(wind1);
+						pvert[j + 1][i].addForce(wind1);
+					}
+				}
+
+
+
+				if (j == 9) { //////BOTTOM SIDE
+					if (i==0) { //Bottom left particle
+						/*Hooke* top = new Hooke(&(pvert[j][i]), &(pvert[j - 1][i]), ks, kd, rest);
+						Hooke* right = new Hooke(&(pvert[j][i]), &(pvert[j][i + 1]), ks, kd, rest);
+
+						pvert[j][i].addForce(&g);
+						pvert[j][i].addForce(top);
+						pvert[j][i].addForce(right);*/
+					}
+					else if (i == 9) { //bottom right particle
+						Hooke* top = new Hooke(&(pvert[j][i]), &(pvert[j - 1][i]), ks, kd, rest);
+						Hooke* left = new Hooke(&(pvert[j][i]), &(pvert[j][i - 1]), ks, kd, rest);
+
+						pvert[j][i].addForce(&g);
+						pvert[j][i].addForce(top);
+						pvert[j][i].addForce(left);
+					}
+					else {
+						Hooke* top = new Hooke(&(pvert[j][i]), &(pvert[j - 1][i]), ks, kd, rest);
+						Hooke* left = new Hooke(&(pvert[j][i]), &(pvert[j][i - 1]), ks, kd, rest);
+						Hooke* right = new Hooke(&(pvert[j][i]), &(pvert[j][i + 1]), ks, kd, rest);
+
+						pvert[j][i].addForce(&g);
+
+						pvert[j][i].addForce(top);
+						pvert[j][i].addForce(left);
+						pvert[j][i].addForce(right);
+					}
+				}
+
+				if (i == 9) { //RIGHT SIDE
+					if (j == 9) { //Bottom right particle
+						/*Hooke* top = new Hooke(&(pvert[j][i]), &(pvert[j - 1][i]), ks, kd, rest);
+						Hooke* left = new Hooke(&(pvert[j][i]), &(pvert[j][i - 1]), ks, kd, rest);*/
+					}
+					else {
+						Hooke* top = new Hooke(&(pvert[j][i]), &(pvert[j - 1][i]), ks, kd, rest);
+						Hooke* left = new Hooke(&(pvert[j][i]), &(pvert[j][i - 1]), ks, kd, rest);
+						Hooke* bottom = new Hooke(&(pvert[j][i]), &(pvert[j + 1][i]), ks, kd, rest);
+
+						pvert[j][i].addForce(&g);
+
+						pvert[j][i].addForce(top);
+						pvert[j][i].addForce(left);
+						pvert[j][i].addForce(bottom);
+					}		
+				}
+
+
+				if (i != 0 && j != 0 && i != 9 && j != 9) {  //INSIDE PARTICLES
+					Hooke* top = new Hooke(&(pvert[j][i]), &(pvert[j - 1][i]), ks, kd, rest);
+					Hooke* left = new Hooke(&(pvert[j][i]), &(pvert[j][i - 1]), ks, kd, rest);
+					Hooke* bottom = new Hooke(&(pvert[j][i]), &(pvert[j + 1][i]), ks, kd, rest);
+					Hooke* right = new Hooke(&(pvert[j][i]), &(pvert[j][i + 1]), ks, kd, rest);
+
+					pvert[j][i].addForce(&g);
+
+					pvert[j][i].addForce(top);
+					pvert[j][i].addForce(left);
+					pvert[j][i].addForce(bottom);
+					pvert[j][i].addForce(right);
+
+					Wind * wind1 = new Wind(&(pvert[j][i]), &(pvert[j][i + 1]), &(pvert[j + 1][i])); //particle, particle right, particle down
+
+					Wind * wind2 = new Wind(&(pvert[j][i+1]), &(pvert[j+1][i+1]), &(pvert[j+1][i])); //particle right, particle right and down, particle down
+
+					pvert[j][i].addForce(wind1);
+					pvert[j][i + 1].addForce(wind1);
+					pvert[j + 1][i].addForce(wind1);
+
+					pvert[j][i+1].addForce(wind2);
+					pvert[j+1][i +1].addForce(wind2);
+					pvert[j + 1][i].addForce(wind2);
+
+				}
+
+			}
+
+
+
+		}
+	}
+}
+
+
+// main function
+int main()
+{
+	// create application
+	Application app = Application::Application();
+	app.initRender();
+	Application::camera.setCameraPosition(glm::vec3(0.0f, 5.0f, 20.0f));
+
+	// create ground plane
+	Mesh plane = Mesh::Mesh(Mesh::QUAD);
+	// scale it up x5
+	plane.scale(glm::vec3(5.0f, 5.0f, 5.0f));
+	Shader lambert = Shader("resources/shaders/physics.vert", "resources/shaders/physics.frag");
+	plane.setShader(lambert);
+
+	Shader blue = Shader("resources/shaders/solid.vert", "resources/shaders/solid_blue.frag");
+	//Create particles via class:
+	//std::vector<Particle> allPart;
+	//for (unsigned int i = 0; i < particlenum; ++i) {
+	//	allPart.push_back(Particle::Particle());
+	//	allPart[i].getMesh().setShader(blue);
+	//}
+
+
+	/*
+	////////////////////////////////////////Create vertice of vertices
+	std::vector<std::vector<Particle>> vecvec(vertnum);
+	for (unsigned int j = 0; j < particlenum; ++j) {
+		for (unsigned int i = 0; i < vertnum; ++i) {
+			vecvec[j].push_back(Particle::Particle());
+			vecvec[j][i].getMesh().setShader(blue);
+		}
+	}
+
+	*/
+
+
+
+
+	//Set particles initial velocity
+	//for (unsigned int j = 0; j < particlenum; ++j) {
+	//	for (unsigned int i = 0; i < vertnum; ++i) {
+	//		//vecvec[j][i].setVel(vec3(RandomFloat(-10.0f, 10.0f), RandomFloat(-10.0f, 10.0f), RandomFloat(-10.0f, 8.0f)));
+	//		vecvec[j][i].setVel(vec3(0.0f));
+	//		//vecvec[j][i].setPos(vec3(RandomFloat(0.0f, 5.0f), RandomFloat(5.0f, 9.0f), RandomFloat(0.0f, 8.0f)));
+	//	}
+	//}
+
+
+	//Set particles initial position
+	//seflagPos(vecvec); 
+	//seflagPoscloth(vecvec);
+
+	//addHookeForces(vecvec);
+	//addClothForces(vecvec);
+
+	// create demo objects (a cube and a sphere)
+	Mesh sphere = Mesh::Mesh("resources/models/sphere.obj");
+	sphere.translate(glm::vec3(-1.0f, 1.0f, 0.0f));
+	sphere.setShader(lambert);
+	Mesh cube = Mesh::Mesh("resources/models/cube.obj");
+	cube.translate(glm::vec3(1.0f, .5f, 0.0f));
+	cube.setShader(lambert);
+
+	//Set up a cubic rigid body
+	RigidBody rb = RigidBody();
+	Mesh m = Mesh::Mesh(Mesh::CUBE);
+	rb.setMesh(m);
+	Shader rbShader = Shader("resources/shaders/physics.vert","resources/shaders/physics.frag");
+	rb.getMesh().setShader(rbShader);
+
+
+	// rigid body motion values
+	rb.translate(vec3(0.0f, 5.0f, 0.0f));
+	rb.setVel(vec3(0.0f, 2.0f, 0.0f));
+	rb.setAngVel(vec3(0.0f, 2.0f, 0.0f));
+
+	//add forces to Rigid body
+	rb.addForce(&g);
+
+
+
+
+	// time
+	GLfloat firstFrame = (GLfloat)glfwGetTime();
+
+
+	//Timestep variables
+	float t = 0.0f;
+	const float dt = 0.01f;
+	float currentTime = (GLfloat)glfwGetTime();
+	float accumulator = 0.0f;
+
+	//Cube Variables
+	glm::vec3 cubecorner = glm::vec3(-5.0f, 0.0f, -5.0f);
+	glm::vec3 d = glm::vec3(10.0f);
+
+
+
+	//Hooke force
+	const int HookeForces = (particlenum*vertnum)-1;
+	std::vector<Hooke*> Hookes;
+
+	// Game loop
+	while (!glfwWindowShouldClose(app.getWindow()))
+	{
+		float newTime = (GLfloat)glfwGetTime();
+		float frameTime = newTime - currentTime;
+		currentTime = newTime;
+
+		accumulator += frameTime;
+
+		/*
+		**	INTERACTION
+		*/
+		// Manage interaction
+		app.doMovement(dt);
+
+		while (accumulator >= dt) {
+			/*
+			**	SIMULATION
+			*/
+			for (unsigned int j = 0; j < vertnum; j++) {
+				for (unsigned int i = 0; i < particlenum; i++) {
+
+					//vecvec[j][i].setAcc(vecvec[j][i].applyForces(vecvec[j][i].getPos(), vecvec[j][i].getVel(), t, dt));
+
+					//for (unsigned int k = 0; k < 3; k++) {
+					//	if (vecvec[j][i].getPos()[k] < cubecorner[k]) {
+					//		vecvec[j][i].setVel(k, vecvec[j][i].getVel()[k] * -1.0f);
+					//		vecvec[j][i].setPos(k, cubecorner[k]);
+					//	}
+					//	else if (vecvec[j][i].getPos()[k] > cubecorner[k] + d[k]) {
+					//		vecvec[j][i].setVel(k, vecvec[j][i].getVel()[k] * -1.0f);
+					//		vecvec[j][i].setPos(k, cubecorner[k] + d[k]);
+					//	}
+
+					//}
+				}
+			}
+
+			for (unsigned int j = 0; j < vertnum; j++) {
+				for (unsigned int i = 0; i < particlenum; i++) {
+
+					//vecvec[j][i].setVel(vecvec[j][i].getVel() + dt * vecvec[j][i].getAcc());
+					//vecvec[j][i].translate(dt*vecvec[j][i].getVel());
+				}
+			}
+
+
+			accumulator -= dt;
+			t += dt;
+		}
+
+		/*
+		**	RENDER
+		*/
+		// clear buffer
+		app.clear();
+		// draw groud plane
+		app.draw(plane);
+		// draw particles
+		//app.draw(particle1.getMesh());
+		//for (unsigned int j = 0; j < vertnum; j++) {
+		//	for (unsigned int i = 0; i < particlenum; i++) {
+		//		app.draw(vecvec[j][i].getMesh());
+		//	}
+		//}
+		// draw demo objects
+		//app.draw(cube);
+		//app.draw(sphere);
+		app.draw(rb.getMesh());
+		app.display();
+	}
+
+	app.terminate();
+
+	return EXIT_SUCCESS;
+}
+
